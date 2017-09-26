8a57eb23f27f5f188e352df298c5523e69301f7a
Philip Steinbrecher <Steinbrp@students.uni-marburg.de>
2017-09-26 19:45:05 +0200

    removed unused shebang

--- bash/module.sh
+++ bash/module.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 _symlink_file $dotfiles_root/bash/.bash_aliases ~/.bash_aliases
 _symlink_file $dotfiles_root/bash/.bash_logout ~/.bash_logout
 _symlink_file $dotfiles_root/bash/.bashrc ~/.bashrc
