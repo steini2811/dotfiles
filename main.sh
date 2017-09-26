c95d3782dd1e1c9f5df365e7823fdc7d2ed139bc
Philip Steinbrecher <Steinbrp@students.uni-marburg.de>
2017-09-26 19:42:43 +0200

    creating config is now handled in main.sh

--- main.sh
+++ main.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
+source "util/logger.sh"
+
+if [ -e config.conf ]; then
+    cp util/config.std config.conf
+    if [ $? -ne 0 ]; then
+        _log_error "unable to create config file"
+        exit 1
+    fi
+    _log_ok "config created - please edit config.conf to fit your preferences"
+    exit 0
+fi
+
 dotfiles_root=$(pwd)
 
 source "config.conf"
-source "util/logger.sh"
 source "util/functions.sh"
 
 for DIR in ./*/
