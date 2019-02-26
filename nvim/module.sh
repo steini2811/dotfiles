f139b400c5402065dd3d38244823459f24e30200
Philip Steinbrecher <philip.steinbrecher@gmx.de>
2019-02-26 17:33:46 +0100

    repairs broken nvim install

--- nvim/module.sh
+++ nvim/module.sh
@@ -1,12 +1,17 @@
 if ! available nvim; then
+    sudo add-apt-repository ppa:neovim-ppa/stable -y
+    sudo apt update
+    sudo apt-get install python-dev python-pip python3-dev python3-pip
     sudo apt install neovim -y
+    sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+    sudo update-alternatives --config vim
 fi
 
 _symlink_file $dotfiles_root/nvim/.vimrc ~/.vimrc
 mkdir --parents ~/.config/nvim
 _symlink_file $dotfiles_root/nvim/.vimrc ~/.config/nvim/init.vim
 
-#curl -Lso "\~/.config/nvim/autoload/plug.vim" --create-dirs "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+curl -Lso ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 nvim +PlugUpgrade +PlugInstall +PlugUpdate +qall
 
