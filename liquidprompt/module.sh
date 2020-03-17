f2481c586ad4f61c46d723e85695ca169d488b07
Philip Steinbrecher <philip.steinbrecher@gmx.de>
2020-03-17 18:06:16 +0100

    adding submodule clone

--- liquidprompt/module.sh
+++ liquidprompt/module.sh
@@ -1,3 +1,5 @@
+git submodule update --init --recursive
+
 _symlink_file $dotfiles_root/liquidprompt/liquidprompt ~/liquidprompt
 mkdir ~/.config
 _symlink_file $dotfiles_root/liquidprompt/liquidpromptrc ~/.config/liquidpromptrc
